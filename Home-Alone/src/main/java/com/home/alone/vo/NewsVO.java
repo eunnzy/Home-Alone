0ac9d25368bcefd2b3d5f8bb51d1caf59362ba8e
eunnzy <silverg12@skuniv.ac.kr>
2023-04-09 19:42:43 +0900

    [부동산 뉴스 크롤링] 페이징 처리했지만, 수정이 필요함

--- Home-Alone/src/main/java/com/home/alone/vo/NewsVO.java
+++ Home-Alone/src/main/java/com/home/alone/vo/NewsVO.java
@@ -8,10 +8,11 @@ import lombok.NoArgsConstructor;
 
 @Data	
 @NoArgsConstructor
-public class PropertyNewsVO {
+public class NewsVO {
 	private String imgUrl;
 	private String title;
 	private String url;
+	private String content;
 	private String publication;	// 신문사
 	private int page;
 }
